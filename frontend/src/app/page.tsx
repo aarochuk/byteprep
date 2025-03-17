4fc6c5249b2852af7f5c1de09ff0111a34f73f4f
aarochuk <andrewarochukwu@gmail.com>
2025-03-17 17:12:52 -0400

    created main stylesheet

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,15 +7,20 @@ export default function Home() {
       <header>
         <p>iBuddy</p>
         <div>
-          <p>Sign up</p>
-          <p>Login</p>
+          <Button>Sign up</Button>
         </div>
       </header>
       <section>
-        <p>Practice for your technical and behavioural interview using ai.</p>
+        <p>your personal ai interview coach</p>
+        <p>Become confident for your next technical or behavioural interview using ai.</p>
+        <p>Practice with interview questions tailored to the job description and the company you are interviewing for.</p>
         <Button>Get started with Google</Button>        
         <Button>Get started with Github</Button>        
       </section>
+      <footer>
+        <p>Andrew Arochukwu &copy; 2025</p>
+        <p>Github</p>
+      </footer>
     </div>
   );
 }
